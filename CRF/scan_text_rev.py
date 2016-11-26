2fd125cf0cffe5715400b784a44671f1a258c70e
Chris Hench <chris.l.hench@gmail.com>
2016-11-25 20:45:28 -0600

    updated CRF constraints

--- CRF/scan_text_rev.py
+++ CRF/scan_text_rev.py
@@ -36,39 +36,66 @@ def only_four_stresses(lines_w_features, tagger, sylls):
         stress = 0
         for i2, l in enumerate(t_line):
 
-            # no doppel can be light
-            if l == "DOPPEL" and syllableweight(line_sylls[i2]) == "L":
-                # unaccented so it get sent to recalculate without doppel
-                stress += 5
-
+                # increment stress
             if t_line[i2] in stressed:
                 stress += 1
 
-            if i2 > 0 and l == "MORA_HAUPT":  # rule out if two
-                if t_line[i2 - 1] == "MORA_HAUPT":
+            # no doppel can be light
+            if l == "DOPPEL" and syllableweight(line_sylls[i2]) == "L":
+                stress += 5  # auto sends to reweight probs
+
+            # no halb-haupt can be heavy
+            if l == "HALB_HAUPT" and syllableweight(line_sylls[i2]) == "H":
+                stress += 5  # auto sends to reweight probs
+
+            # no EL can be heavy
+            if l == "EL" and syllableweight(line_sylls[i2]) == "H":
+                stress += 5  # auto sends to reweight probs
+
+            # error alternation, recount
+            accs = ["MORA_HAUPT", "HALB_HAUPT"]
+            if i2 > 0 and l in accs:  # rule out if two
+                if t_line[i2 - 1] in accs:
                     stress += 5
 
+            # rule out if no stress following doppel
             if i2 < len(t_line) - 1 and l == "DOPPEL":
                 if t_line[i2 + 1] not in stressed:
                     stress += 5
 
+            if i2 > 0 and l == "DOPPEL":  # rule out stress before double
+                if t_line[i2 - 1] in accs:
+                    stress += 5
+
+        # if > 4 stresses, look at probs
         if stress != 4:
             line_probs = []
             for i3, l in enumerate(t_line):
+
+                # marginal probablities
                 probs = [(l, tagger.marginal(l, i3)) for l in labs]
 
                 # no doppel can be light
                 if syllableweight(line_sylls[i3]) == "L":
                     probs = [x for x in probs if x[0] != "DOPPEL"]
 
+                # no halbhaupt or EL can be heavy
+                if syllableweight(line_sylls[i3]) == "H":
+                    probs = [x for x in probs if x[0] != "HALB_HAUPT"]
+                    probs = [x for x in probs if x[0] != "EL"]
+
                 probs = sorted(probs, key=lambda tup: tup[1], reverse=True)
+
+                # if very certain, only take top 2, otherwise top 4
                 if probs[0][1] > .9:
                     line_probs.append(probs[:2])
                 else:
                     line_probs.append(probs[:4])
 
+            # get combinations of syll values with probs
             combos = itertools.product(*line_probs)
 
+            # verify each combo and rank
             final_line = (0, 0)
             for c in combos:
                 stress = 0
@@ -82,8 +109,14 @@ def only_four_stresses(lines_w_features, tagger, sylls):
                         if c[i4 + 1][0] not in stressed:
                             stress += 5
 
-                    if i4 > 0 and l[0] == "MORA_HAUPT":  # rule out if two
-                        if c[i4 - 1][0] == "MORA_HAUPT":
+                    # error alternation recount
+                    accs = ["MORA_HAUPT", "HALB_HAUPT"]
+                    if i4 > 0 and l[0] in accs:  # rule out if two
+                        if c[i4 - 1][0] in accs:
+                            stress += 5
+
+                    if i4 > 0 and l[0] == "DOPPEL":  # no stress b4 double
+                        if c[i4 - 1][0] in accs:
                             stress += 5
 
                 if stress == 4 and tot_prob > final_line[1]:
@@ -92,9 +125,9 @@ def only_four_stresses(lines_w_features, tagger, sylls):
             try:
                 t_line = [x[0] for x in final_line[0]]
             except TypeError:
-                pass
+                pass  # not continue, pass will do nothing
 
-        four_stress.append(t_line)
+        four_stress.append(t_line)  # will take orig if no errors
 
     # additional fixes
     final_labels = []
@@ -168,7 +201,7 @@ def only_four_stresses(lines_w_features, tagger, sylls):
 
     final_labels = final_labels2
 
-    # # fix endings with light halves
+    # # zweisilbig maennlich kadenz
     # final_labels2 = []
     # prefixes = ["ge", "be", "en"]
     # for i, line in enumerate(final_labels):
